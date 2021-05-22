5b680d07c7176e7fb465ef4c45766c8d2e08ab91
Yann Braga <yannbf@gmail.com>
2021-05-22 22:52:44 +0200

    add msw

--- src/components/Login.test.jsx
+++ src/components/Login.test.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { mount } from "@cypress/react";
-import Login from "./Login";
+import { setupWorker } from 'msw'
 
-describe("<Login>", () => {
-  it("playground", () => {
-    mount(<Login />);
-    cy.get("input[type!=password]").type("strasbourg");
-    cy.get("input[type=password]").type("p@ssw0rd!");
-    cy.get("button").click();
-    cy.contains("Welcome strasbourg").should("be.visible");
-  });
-});
+describe('<Login>', () => {
+  // eslint-disable-next-line no-undef
+  before(() => {
+    setupWorker()
+    // commenting the method start makes the tests run fine. keeping it brings to weird test loop
+    .start({
+      serviceWorker: {
+        url: '/public/mockServiceWorker.js',
+      }
+    })
+  })
+
+  it('Test 1', () => {
+    mount(<button>Hello</button>) 
+      .get('button')
+      .should('exist')
+  })
+
+  it('Test 2', () => {
+    mount(<button>Something went wrong!</button>) 
+      .get('body')
+      .findByText('Something went wrong!').should('exist')
+  })
+})
